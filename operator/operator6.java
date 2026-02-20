23aca9108df411eb68cefc9dddea75ee8284f83d
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-20 21:59:55 +0530

    Add files via upload
    operator6.java

---
+++ operator/operator6.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Scholarship {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int income = sc.nextInt();
+        int percentage = sc.nextInt();
+        if (income < 200000 && percentage >= 75) {
+            System.out.println("Granted");
+        } else {
+            System.out.println("Not Granted");
+        }
+        sc.close();
+    }
+}
