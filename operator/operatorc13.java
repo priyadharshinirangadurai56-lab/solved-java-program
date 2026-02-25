d3c5cd6985cb9009547241ff58c9692878e042fe
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-25 21:09:29 +0530

    Add files via upload
    operatorc13.java

---
+++ operator/operatorc13.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ApplianceStatus {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int connectionValue = sc.nextInt();
+        int applianceMask = sc.nextInt();
+        if ((connectionValue & applianceMask) != 0) {
+            System.out.println("Appliance Active");
+        } else {
+            System.out.println("Appliance Inactive");
+        }
+    }
+}
