bba99dafda6ae79b360e245562f8fd2bb08bc801
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-02-25 21:05:47 +0530

    Add files via upload
    operator14.c

---
+++ operator/operator14.c
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class PermissionMerge {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int permissionA = sc.nextInt();
+        int permissionB = sc.nextInt();
+        int mergedPermission = permissionA | permissionB;
+        System.out.println("Merged Permission Code = " + mergedPermission);
+    }
+}
