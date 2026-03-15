d9fed548740099384da874aa66fac15297d37765
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-03-15 18:10:56 +0530

    Add files via upload
    whileloop 1.java

---
+++ while loop/whileloop 1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class PasswordValidator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String correctPassword = sc.nextLine();
+        int attempts = 3;
+        while (attempts > 0) {
+            System.out.println("Enter password:");
+            String userPassword = sc.nextLine();
+            if (userPassword.equals(correctPassword)) {
+                System.out.println("Access granted!");
+                break;
+            } 
+            else {
+                attempts--;
+                if (attempts == 0) {
+                    System.out.println("Account locked!");
+                } 
+                else {
+                    System.out.println("Incorrect! Attempts remaining: " + attempts);
+                }
+            }
+        }
+    }
+}
