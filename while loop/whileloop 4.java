7773dc0bb170aa217957ba733b1569a06fe0ff2b
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-03-16 18:56:35 +0530

    Add files via upload
    whileloop 4.java

---
+++ while loop/whileloop 4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class FactorialWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in)
+        int n = sc.nextInt();
+        long result = 1;
+        int counter = 1;
+        while (counter <= n) {
+            result = result * counter;
+            counter++;
+        }
+        System.out.println("Factorial of " + n + " = " + result);
+    }
+}
