51949fd442842bd8829f06c10e4e4103494bc1eb
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-04-02 18:35:10 +0530

    Add files via upload
    while loop 24.java

---
+++ while loop/while loop 24.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class HCFSubtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int x = a;
+        int y = b;
+        while (x != y) {
+            if (x > y) {
+                x = x - y;
+            } else {
+                y = y - x;
+            }
+        }
+        System.out.println("HCF of " + a + " and " + b + " = " + x);
+    }
+}
