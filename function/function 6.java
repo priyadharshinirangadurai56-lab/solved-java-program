75be313e02ec26ec72e022b0a6d07fac7d4ad4c4
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-05-04 10:18:57 +0530

    Add files via upload
    function 6.java

---
+++ function/function 6.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    public static long factorial(int n) {
+        long fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
