46d5dd6d2b922c0a9dec2c4fbb0eb12508e2735d
priyadharshinirangadurai56-lab <priyadharshinirangadurai56@gmail.com>
2026-05-23 19:21:32 +0530

    Add files via upload
    function 18.java

---
+++ function/function 18.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+    public static int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(gcd(A, B));
+    }
+}
